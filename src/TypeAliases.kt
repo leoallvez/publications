ed27c922504c6012ca0143cfd5992aa6c1f21af7
Leonardo <leoallvez@hotmail.com>
2021-02-12 23:54:43 -0300

    typealias

---
+++ src/TypeAliases.kt
@@ -0,0 +1,6 @@
+
+//Os aliases de tipo fornecem nomes alternativos para os tipos existentes. 
+//Se o nome do tipo for muito longo, você pode inserir um nome mais curto diferente e usar o novo.
+
+//Considere o tipo a seguir
+typealias Callback = (List<Int>) -> Unit
