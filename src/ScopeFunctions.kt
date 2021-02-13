5276c0aab3ce3b319901164ebd6154ebf45c1bca
Leonardo <leoallvez@hotmail.com>
2021-02-13 02:10:14 -0300

    typealias

--- src/ScopeFunctions.kt
+++ src/ScopeFunctions.kt
@@ -1,5 +1,18 @@
+//funções de escopo
 
 
-fun fun main() {
-    
+//Kotlin contém várias funções cujo único propósito é executar um bloco de código dentro do contexto de um objeto
+//Quando você chama essa função em um objeto com uma expressão lambda fornecida, ela forma um escopo temporário
+
+fun myFun() {
+    val outside = 6.2831853071
+    run {
+        val inside = 1.61803398875
+        // Both outside and inside are usable and in scope
+    }
+    // inside is out of scope, and only outside is available
+}
+
+fun main() {
+    print(myFun())
 }
--- src/TypeAliases.kt
+++ src/TypeAliases.kt
@@ -3,4 +3,16 @@
 //Se o nome do tipo for muito longo, você pode inserir um nome mais curto diferente e usar o novo.
 
 //Considere o tipo a seguir
-typealias Callback = (List<Int>) -> Unit
+typealias Callback = (Map<String, Double>) -> Unit
+
+fun doMap() = mapOf("Um" to 1.0, "Um e meio" to 1.5, "Dois" to 2.0)
+
+fun makeMapV1(callback: (Map<String, Double>) -> Unit) = callback(doMap())
+
+fun makeMapV2(callback: Callback) = callback(doMap())
+
+fun main() {
+    makeMapV2 { 
+        it.forEach { println(it) } 
+    }
+}
