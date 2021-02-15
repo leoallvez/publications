9b92c23ef88e7e339b5307f00a078f08b7a56a59
Leonardo <leoallvez@hotmail.com>
2021-02-14 22:35:14 -0300

    Edit typealias

--- src/TypeAliases.kt
+++ src/TypeAliases.kt
@@ -1,15 +1,21 @@
 
-//Os aliases de tipo fornecem nomes alternativos para os tipos existentes. 
-//Se o nome do tipo for muito longo, você pode inserir um nome mais curto diferente e usar o novo.
+//Type aliases fornecem nomes alternativos para os tipos existentes. 
 
-//Considere o tipo a seguir
-typealias Callback = (Map<String, Double>) -> Unit
+//Se o nome do tipo for muito longo, você pode declarar um nome mais curto diferente e usar o novo.
 
-fun doMap() = mapOf("Um" to 1.0, "Um e meio" to 1.5, "Dois" to 2.0)
-
+//Considere o método a seguir, com um parâmetro (função callback) um pouco extenso.
 fun makeMapV1(callback: (Map<String, Double>) -> Unit) = callback(doMap())
 
+//Podemos declarar um type aliase com o callback.
+typealias Callback = (Map<String, Double>) -> Unit
+
+//Usando o type aliases o mesmo método fica mais legível e sucinto.
 fun makeMapV2(callback: Callback) = callback(doMap())
+
+//Se você usar o mesmo tipo em vários lugares pode ser uma boa alternativa o uso de type aliase,
+//assim você poderia centralizar alterações.
+
+fun doMap() = mapOf("Um" to 1.0, "Um e meio" to 1.5, "Dois" to 2.0)
 
 fun main() {
     makeMapV2 { 
